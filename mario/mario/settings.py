2b6ea72abc070316cfe1d724faa17c65f654da13
spacetroopers <sulistyoardani@gmail.com>
2019-02-06 05:15:59 +0700

    a

--- mario/mario/settings.py
+++ mario/mario/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = True
 
 ALLOWED_HOSTS = [''0.0.0.0', 'localhost', '127.0.0.1','skripsi-deploy.herokuapp.com'']
 
