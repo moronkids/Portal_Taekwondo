fe53bd87cbab333b9ed0ff83516e7ee6787cfc90
spacetroopers <sulistyoardani@gmail.com>
2019-02-06 02:07:51 +0700

    switch to postgresql

--- mario/login_auth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LoginAuthConfig(AppConfig):
-    name = 'login_auth'
--- mario/mario/settings.py
+++ mario/mario/settings.py
@@ -36,13 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'zwinkle',
-    'login_auth',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'bootstrap4',
     'widget_tweaks',
-]
+    'django_filters',
+    ]
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
@@ -91,11 +90,22 @@ WSGI_APPLICATION = 'mario.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
