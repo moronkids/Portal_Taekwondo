cad5584a5ef59e0c3ca9de5d5c917e9fa5a83b5a
spacetroopers <sulistyoardani@gmail.com>
2019-02-06 18:25:59 +0700

    sdsfdcvf

--- mario/zwinkle/urls.py
+++ mario/zwinkle/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-from zwinkle import views
+from mario.zwinkle import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
