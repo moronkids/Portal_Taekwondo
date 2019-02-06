229ebf07ed5ae1c51f4588923584f0c6a06f4840
spacetroopers <sulistyoardani@gmail.com>
2019-02-06 16:53:58 +0700

    ssd

--- mario/mario/wsgi.py
+++ mario/mario/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mario.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mario.mario.settings')
 
 application = get_wsgi_application()
 application = get_wsgi_application()
