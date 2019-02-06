7196212b72c7383b1bc3e45056761d39c57a03ab
spacetroopers <sulistyoardani@gmail.com>
2019-02-06 18:40:31 +0700

    spqo

--- mario/zwinkle/urls.py
+++ mario/zwinkle/urls.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
-
-
 from mario.zwinkle import views
 
 urlpatterns = [
