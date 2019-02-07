ffaaa615e5fff7283dbb1d0997850032c8cb1d01
spacetroopers <sulistyoardani@gmail.com>
2019-02-07 22:49:24 +0700

    sfddf

--- mario/mario/wsgi.py
+++ mario/mario/wsgi.py
@@ -12,7 +12,5 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mario.mario.settings')
-
-application = get_wsgi_application()
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
