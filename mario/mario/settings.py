ca7a7a79d9b3000c09886ef550c4bd20bf59f3d2
spacetroopers <sulistyoardani@gmail.com>
2019-02-08 00:22:42 +0700

    KONTOLxx

--- mario/mario/settings.py
+++ mario/mario/settings.py
@@ -62,8 +62,9 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
